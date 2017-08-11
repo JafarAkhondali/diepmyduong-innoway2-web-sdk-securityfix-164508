5b08ad5491f2bfceef90e6c4fe8208aecb5cc0fa
admin <admin@admins-MacBook-Pro.local>
2017-08-11 14:17:02 +0700

    update sdk

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,13 @@
 import { Product } from './product';
 import { Customer } from './customer';
+import { Topping } from './topping';
+import { ToppingValue } from './topping_value';
+import { ProductTopping } from './product_topping';
+import { Attribute } from './attribute';
+import { ProductAttribute } from './product_attribute';
+import { ProductType } from './product_type';
+import { ProductCategory } from './product_category';
+import { ProductStock } from './product_stock';
 
 //Khai báo module được sử dụng
 const classes = { 
