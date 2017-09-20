133228860a76e9382e5968650c83ecf0e9467011
diepmyduong <diepmyduong@outlook.com.vn>
2017-09-20 14:41:12 +0700

    Update Auth Module

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,7 +1,9 @@
 import { Base } from './base';
 import { Api } from '../api';
+import { Config } from '../config';
+import { Vendor,Timeout } from '../utils/helper';
 
-declare var $:any;
+declare var $,AccountKit,window:any;
 export class Auth extends Base {
     constructor(){
         super("auth");
@@ -12,6 +14,10 @@ export class Auth extends Base {
 
     private _authState:boolean;
     userInfo:any
+    anonymous_token:string
+    customer_verify_token:string
+    employee_token:string
+    customer_token:string
     
     set authenticated(state:boolean){
         this._authState = state;
@@ -43,7 +49,8 @@ export class Auth extends Base {
         }
         let res:any = await this.exec(settings);
         let row = res.results.object;
-        return row;
+        this.anonymous_token = row.brand_token
+        return this.anonymous_token
     }
 
     async loginWithToken(token:string){
@@ -64,6 +71,9 @@ export class Auth extends Base {
             this.authenticated = true
             return row
         }catch(err){
+            console.log('login with token error',err);
+            let token = await this.getAnonymouseToken(Config.get('brand_id'))
+            Api.setAccessToken(token)
             this.authenticated = false
             return null
         }
@@ -90,6 +100,7 @@ export class Auth extends Base {
             let res:any = await this.exec(settings)
             let row = res.results.object
             this.userInfo = row.user
+            this.employee_token = row.access_token
             Api.setAccessToken(row.access_token)
             this.authenticated = true
             return row
@@ -100,10 +111,107 @@ export class Auth extends Base {
     }
 
     async logout(){
-        var brand_token = await Api.module('brand').getBrandToken();
-        Api.setAccessToken(brand_token);
+        let token = await this.getAnonymouseToken(Config.get('brand_id'))
+        Api.setAccessToken(token);
         this.userInfo = undefined;
         this.authenticated = false;
         return true;
+    }
+
+    private async customerVerifyToken(){
+        let token = await this.getAnonymouseToken(Config.get('brand_id'))
+        let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": this.url('/customer_verify_token'),
+            "headers": {
+                "access_token": token,
+            },
+            "method": "GET"
+        }
+        let res:any = await this.exec(settings);
+        let row = res.results.object;
+        this.customer_verify_token = row.verify_token
+        return this.customer_verify_token
+    }
+
+    async AKitVerify(authStateInfo){
+        if(!this.customer_verify_token) throw new Error('customer_verify_token undefined in AKitVerify')
+        let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": this.url("/customer_verify_phone"),
+            "method": "POST",
+            "headers": {
+                "access_token": this.customer_verify_token,
+                "content-type": "application/json",
+            },
+            "processData": false,
+            "data": JSON.stringify(authStateInfo)
+        }
+        try {
+            let res:any = await this.exec(settings);
+            let row = res.results.object
+            this.userInfo = row.customer
+            this.customer_token = row.access_token
+            Api.setAccessToken(this.customer_token )
+            this.authenticated = true
+            return row
+        }catch(err){
+            this.authenticated = false
+            throw err
+        }
+    }
+
+    async AKitInit(){
+        let token = this.customer_verify_token || await this.customerVerifyToken()
+        let tokenInfo = await Api.module('info').getTokenInfo(token);
+        let options = tokenInfo.payload;
+        if(typeof AccountKit === "undefined" || typeof AccountKit.init === "undefined"){
+            let accountkitSrc = "https://sdk.accountkit.com/"+options.locale+"/sdk.js";
+            await Vendor.loadScript(accountkitSrc,{id: "accoutkit_sdk"});
+            while(typeof AccountKit === "undefined" || typeof AccountKit.init === "undefined" ){
+                await Timeout(500);
+            }
+            AccountKit.init({
+                appId:    options.app_id, 
+                state:    options.state, 
+                version:  options.version,
+                fbAppEventsEnabled:true
+            });
+        }
+        return options;
+    }
+
+    AKitLogin(countryCode,phoneNumber){
+        return new Promise((resolve,reject)=>{
+            AccountKit.login('PHONE', {
+                countryCode: countryCode, 
+                phoneNumber: phoneNumber
+            },(response) => {
+                if (response.status === "PARTIALLY_AUTHENTICATED") {
+                    this.AKitVerify({
+                        state: response.state,
+                        code: response.code
+                    }).then(user =>{
+                        resolve(user);
+                    }).catch(err=>{
+                        reject(err);
+                    })
+                } else if (response.status === "NOT_AUTHENTICATED") {
+                    reject({
+                        code: 500,
+                        type: "NOT_AUTHENTICATED",
+                        message: "NOT AUTHENTICATED"
+                    })
+                }else if (response.status === "BAD_PARAMS") {
+                    reject({
+                        code: 500,
+                        type: "BAD_PARAMS",
+                        message: "BAD PARAMS"
+                    })
+                }
+            });
+        })    
     }
 }
