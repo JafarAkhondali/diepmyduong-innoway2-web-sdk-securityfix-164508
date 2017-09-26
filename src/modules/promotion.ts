2b593a91ea045bd83d7c9b9119e400e5e40dab6d
diepmyduong <diepmyduong@outlook.com.vn>
2017-09-26 12:05:43 +0700

    Validate Promotion

--- src/modules/promotion.ts
+++ src/modules/promotion.ts
@@ -6,4 +6,21 @@ export class Promotion extends Crud {
     	super("promotion");
     }
 
+    async validate(id,customer_id){
+        let access_token = await this.getAccessToken()
+        let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": this.url(`/${id}/validate`,{
+                customer_id
+            }),
+            "headers": {
+                "access_token": access_token,
+            },
+            "method": "GET"
+        }
+        let res:any = await this.exec(settings);
+        return true;
+    }   
+
 }
