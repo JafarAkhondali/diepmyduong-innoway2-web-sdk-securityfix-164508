2a389b87f56288e2a9ae8216a4e99d1f5e9755fb
diepmyduong <diepmyduong@outlook.com.vn>
2017-10-05 14:44:29 +0700

    add changeActivity to bill

--- src/modules/bill.ts
+++ src/modules/bill.ts
@@ -11,6 +11,49 @@ export class Bill extends Crud {
     constructor(){
     	super("bill");
     }
+    
+    ActivityEnum: {
+        BILL_SENT_SUCCESSFULLY: 'BILL_SENT_SUCCESSFULLY',
+        BILL_DISTRIBUTED: 'BILL_DISTRIBUTED',
+    
+        BILL_WAITING_FOR_CONFIRMATION: 'BILL_WAITING_FOR_CONFIRMATION',
+        BILL_MODIFIED_AT_WAITING_FOR_CONFIRMATION: 'BILL_MODIFIED_AT_WAITING_FOR_CONFIRMATION',
+        BILL_CANCELLED_AT_WAITING_FOR_CONFIRMATION: 'BILL_CANCELLED_AT_WAITING_FOR_CONFIRMATION',
+    
+        BILL_CONFIRMED: 'BILL_CONFIRMED',
+        BILL_MODIFIED_AT_BILL_CONFIRMED: 'BILL_MODIFIED_AT_BILL_CONFIRMED',
+        BILL_CANCELLED_AT_BILL_CONFIRMED: 'BILL_CANCELLED_AT_BILL_CONFIRMED',
+    
+        BILL_PICKING_UP: 'BILL_PICKING_UP',
+        BILL_MODIFIED_AT_PICKING_UP: 'BILL_MODIFIED_AT_PICKING_UP',
+        BILL_CANCELLED_AT_PICKING_UP: 'BILL_CANCELLED_AT_PICKING_UP',
+    
+        BILL_RECEIVED: 'BILL_RECEIVED',
+        BILL_MODIFIED_AT_RECEIVED: 'BILL_MODIFIED_AT_RECEIVED',
+        BILL_CANCELLED_AT_RECEIVED: 'BILL_CANCELLED_AT_RECEIVED',
+    
+        BILL_PROCESSING: 'BILL_PROCESSING',
+        BILL_MODIFIED_AT_PROCESSING: 'BILL_MODIFIED_AT_PROCESSING',
+        BILL_CANCELLED_AT_PROCESSING: 'BILL_CANCELLED_AT_PROCESSING',
+    
+        BILL_PREPARED: 'BILL_PREPARED',
+        BILL_MODIFIED_AT_PREPARED: 'BILL_MODIFIED_AT_PREPARED',
+        BILL_CANCELLED_AT_PREPARED: 'BILL_CANCELLED_AT_PREPARED',
+    
+        BILL_SENT_SHIPPER: 'BILL_SENT_SHIPPER',
+        BILL_MODIFIED_AT_SENT_SHIPPER: 'BILL_MODIFIED_AT_SENT_SHIPPER',
+        BILL_CANCELLED_AT_SENT_SHIPPER: 'BILL_CANCELLED_AT_SENT_SHIPPER',
+    
+        BILL_DELIVERING: 'BILL_DELIVERING',
+        BILL_MODIFIED_AT_DELIVERING: 'BILL_MODIFIED_AT_DELIVERING',
+        BILL_CANCELLED_AT_DELIVERING: 'BILL_CANCELLED_AT_DELIVERING',
+    
+        BILL_PAID: 'BILL_PAID',
+        BILL_MODIFIED_AT_PAID: 'BILL_MODIFIED_AT_PAID',
+    
+        BILL_COLLECTED_MONEY: 'BILL_COLLECTED_MONEY',
+        BILL_MODIFIED_AT_COLLECTED_MONEY: 'BILL_MODIFIED_AT_COLLECTED_MONEY'
+    }
 
     async sendBill(data){
         
@@ -84,4 +127,33 @@ export class Bill extends Crud {
             })
         })
     }
+
+    async changeActivity(bill_id,data){
+        let { activity , note } = data
+        const access_token = await this.getAccessToken()
+        let settings:any = {
+            "async": true,
+            "crossDomain": true,
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "access_token": access_token,
+            },
+            "processData": false,
+            "data": JSON.stringify(data)
+        }
+        switch(activity){
+            case this.ActivityEnum.BILL_WAITING_FOR_CONFIRMATION:
+                settings.url = this.url(`/${bill_id}/activity/waitingForConfirmationBillStatus`)
+                break
+            default:
+                throw new Error('This activity not allow for this bill')
+        }
+
+        let res:any = await this.exec(settings);
+        let row = res.results.object;
+        return row;
+    }
+
+    
 }
